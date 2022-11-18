b064b25c33aba5bf473a575ea9e2c1f3260ab4ed
Daniel <dcordobaruiz@gmail.com>
2022-11-18 10:13:46 -0500

    menu desplegable

---
+++ js/media-query.js
@@ -0,0 +1,8 @@
+let barmenu = document.getElementById("barmenu");
+let main_menu = document.getElementById("main-menu");
+
+barmenu.addEventListener("click",function(){
+    if(main_menu.classList.contains(main-menu)) {
+        main_menu.classList.add()
+    }
+})
