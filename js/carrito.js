362e2632ebec60772f657b8891d53affd32aef0f
danielcordoba12 <danielcordobaruiz451@gmail.com>
2023-04-26 23:13:52 -0500

    cariito.js-part-4

--- js/carrito.js
+++ js/carrito.js
@@ -1,12 +1,13 @@
-let imgBox = document.getElementById("imgBox")
-// let boxImgMini1 = document.getElementById("boxImgMini1")
-// let boxImgMini2 = document.getElementById("boxImgMini2")
-// let boxImgMini3 = document.getElementById("boxImgMini3")
-// let boxImgMini4 = document.getElementById("boxImgMini4")
+let imgBox = document.getElementById("imgBox");
 let imgMini1 = document.getElementById("imgMini1");
 let imgMini2 = document.getElementById("imgMini2");
 let imgMini3 = document.getElementById("imgMini3");
 let imgMini4 = document.getElementById("imgMini4");
+
+/* let boxImgBox1 = document.getElementById("boxImgBox1");
+let boxImgBox2 = document.getElementById("boxImgBox2");
+let boxImgBox3 = document.getElementById("boxImgBox3");
+let boxImgBox4 = document.getElementById("boxImgBox4"); */
 
 
 imgMini1.addEventListener("click",function(){
@@ -19,11 +20,10 @@ imgMini1.addEventListener("click",function(){
     boxImgBox2.classList.remove("img-mini-act")
     boxImgBox3.classList.remove("img-mini-act")
     boxImgBox4.classList.remove("img-mini-act")
-});
+})
 
-
-imgMini2.addEventListener("click", function(){
-    imgBox.setAttribute("src","img/image-product-2.jpg");
+imgMini2.addEventListener("click",function(){
+    imgBox.setAttribute("src","img/image-product-2.jpg")
     boxImgBox2.classList.add("box-img-mini")
     boxImgBox1.classList.remove("box-img-mini")
     boxImgBox3.classList.remove("box-img-mini")
@@ -32,9 +32,10 @@ imgMini2.addEventListener("click", function(){
     boxImgBox1.classList.remove("img-mini-act")
     boxImgBox3.classList.remove("img-mini-act")
     boxImgBox4.classList.remove("img-mini-act")
-});
+})
+
 imgMini3.addEventListener("click",function(){
-    imgBox.setAttribute("src", "img/image-product-3.jpg");
+    imgBox.setAttribute("src","img/image-product-3.jpg")
     boxImgBox3.classList.add("box-img-mini")
     boxImgBox1.classList.remove("box-img-mini")
     boxImgBox2.classList.remove("box-img-mini")
@@ -43,9 +44,9 @@ imgMini3.addEventListener("click",function(){
     boxImgBox1.classList.remove("img-mini-act")
     boxImgBox2.classList.remove("img-mini-act")
     boxImgBox4.classList.remove("img-mini-act")
-});
+})
 imgMini4.addEventListener("click",function(){
-    imgBox.setAttribute("src", "img/image-product-4.jpg");
+    imgBox.setAttribute("src","img/image-product-4.jpg")
     boxImgBox4.classList.add("box-img-mini")
     boxImgBox1.classList.remove("box-img-mini")
     boxImgBox2.classList.remove("box-img-mini")
@@ -54,66 +55,154 @@ imgMini4.addEventListener("click",function(){
     boxImgBox1.classList.remove("img-mini-act")
     boxImgBox2.classList.remove("img-mini-act")
     boxImgBox3.classList.remove("img-mini-act")
-});
-// Botones para aumnetar y disminuir compra
+})
 
-let btnRestar = document.getElementById("btnRestar");
-let btnCantidad = document.getElementById("btnCantidad")
-let btnSumar = document.getElementById("btnSumar");
-let cantBox = document.getElementById("cantBox");
-let numeroElementos = document.getElementById("numeroElementos");
-// Valor unitario total
+let btnRestar = document.getElementById("subOb");
+let canBox = document.getElementById("cantBox");
+let btnCantidad = document.getElementById("canOb");
+let btnSumar =document.getElementById("addOb");
+let numElem = document.getElementById("numeroElementos");
+//valor total
 let valorUnitario = 199000;
 let valorUnitarioTxt = valorUnitario.toString();
-// P.O.O -> Programacion orientada objetos
-let valorUnitarioPeso = new Intl.NumberFormat().format(valorUnitario);
 
+//Programació Orientada a Objetos}
+
+let valoeUnitarioPeso = new Intl.NumberFormat().format(valorUnitario);
+
+// Identificar cantidad de elementos
+let cantNumTotal = valorUnitarioTxt.length;
 
 let valorTotal = document.getElementById("valorTotal");
-// Inicializando la variable
-let valorTotalInt = 0; 
-// let numeroElementostxt = document.getElementById("numeroElementos").innerHTML;
+let valorTotalInt= 0; 
+
 
 btnSumar.addEventListener("click",function(){
-    // nodeToAdd.appendChild(nodeText);
-    // btnCantidad.appendChild(nodeToAdd);
-    let numeroElementosTxt = document.getElementById("numeroElementos").innerHTML;
+    let numElementosTxt = document.getElementById("numeroElementos").innerHTML;
+    let numeroEnterosInt = parseInt(numElementosTxt);
+    
 
-    let numeroElementosInt = parseInt(numeroElementosTxt);
-    if (numeroElementosInt < 10) {
-        let totalElementosInt = numeroElementosInt + 1 ;
-        let totalElementostxt = totalElementosInt.toString();
-        numeroElementos.innerHTML = totalElementostxt;
-        // ----------------------------------------------------
-        valorTotalInt = valorUnitario * totalElementosInt;
-        valorTotal.innerHTML = "$"  + new Intl.NumberFormat().format(valorTotalInt);
-        
 
+    if (numeroEnterosInt<=9) {
+        let totalElementosInt= numeroEnterosInt + 1;
+        let totalElementosTxt= totalElementosInt.toString();
+        numElem.innerHTML= totalElementosTxt;
+        valorTotalInt = valorUnitario*totalElementosInt;
+        valorTotal.innerHTML = "$ " + new Intl.NumberFormat().format(valorTotalInt);
+    } else {
+        numElem.innerHTML= totalElementosTxt;
     }
-
-
-});
-btnRestar.addEventListener("click",function(){
-
-    if(numeroElementos.innerHTML > 0){
-        let numeroElementosTxt = document.getElementById("numeroElementos").innerHTML;
-        let numeroElementosInt = parseInt(numeroElementosTxt);
-        let totalElementosInt = numeroElementosInt - 1 ;
-        let totalElementostxt = totalElementosInt.toString();
-        numeroElementos.innerHTML = totalElementostxt;
-        //----------------------------
-        valorTotalInt = valorUnitario * totalElementosInt;
-        valorTotal.innerHTML = "$"  + new Intl.NumberFormat().format(valorTotalInt);
-    };
+    
 });
 
-//----------------------------------------x------------------------
-let lightBox = document.getElementById("lightBox")
+
+btnRestar.addEventListener("click", function(){
+    let numElementosTxt = document.getElementById("numeroElementos").innerHTML;
+    let numeroEnterosInt = parseInt(numElementosTxt);
+
+    if (numeroEnterosInt>=1) {
+        let totalElementosInt= numeroEnterosInt - 1;
+        let totalElementosTxt= totalElementosInt.toString();
+        numElem.innerHTML= totalElementosTxt;
+        valorTotalInt = valorUnitario*totalElementosInt;
+        valorTotal.innerHTML = "$ " + new Intl.NumberFormat().format(valorTotalInt);
+        if (valorTotalInt<=0){
+            valorTotal.innerHTML = "$ " + new Intl.NumberFormat().format(valorUnitario);
+        }
+    }
+    else {
+        numElem.innerHTML= totalElementosTxt;
+    }
+    
+});
+
+let lightBox =document.getElementById("lightBox");
+let btnX = document.getElementById("btnX");
 
 imgBox.addEventListener("click",function(){
-    lightBox.classList.remove("light-hidden");
+    lightBox.classList.remove("lightbox-hidden");
+})
 
-});
+btnX.addEventListener("click",function(){
+   lightBox.classList.add("lightbox-hidden");
+})
+
+let imgEscPrin = document.getElementById("imgEscPrin");
+let imgMiniEsc1 = document.getElementById("imgMiniEsc1");
+let imgMiniEsc2 = document.getElementById("imgMiniEsc2");
+let imgMiniEsc3 = document.getElementById("imgMiniEsc3");
+let imgMiniEsc4 = document.getElementById("imgMiniEsc4");
+
+imgMiniEsc1.addEventListener("click",function(){
+    imgEscPrin.setAttribute("src","img/image-product-1.jpg")
+    imgEsc1.classList.add("box-img-mini")
+    imgEsc2.classList.remove("box-img-mini")
+    imgEsc3.classList.remove("box-img-mini")
+    imgEsc4.classList.remove("box-img-mini")
+    imgEsc1.classList.add("img-mini-act")
+    imgEsc2.classList.remove("img-mini-act")
+    imgEsc3.classList.remove("img-mini-act")
+    imgEsc4.classList.remove("img-mini-act")
+})
+
+imgMiniEsc2.addEventListener("click",function(){
+    imgEscPrin.setAttribute("src","img/image-product-2.jpg")
+    imgEsc2.classList.add("box-img-mini")
+    imgEsc1.classList.remove("box-img-mini")
+    imgEsc3.classList.remove("box-img-mini")
+    imgEsc4.classList.remove("box-img-mini")
+    imgEsc2.classList.add("img-mini-act")
+    imgEsc1.classList.remove("img-mini-act")
+    imgEsc3.classList.remove("img-mini-act")
+    imgEsc4.classList.remove("img-mini-act")
+})
+
+imgMiniEsc3.addEventListener("click",function(){
+    imgEscPrin.setAttribute("src","img/image-product-3.jpg")
+    imgEsc3.classList.add("box-img-mini")
+    imgEsc1.classList.remove("box-img-mini")
+    imgEsc2.classList.remove("box-img-mini")
+    imgEsc4.classList.remove("box-img-mini")
+    imgEsc3.classList.add("img-mini-act")
+    imgEsc1.classList.remove("img-mini-act")
+    imgEsc2.classList.remove("img-mini-act")
+    imgEsc4.classList.remove("img-mini-act")
+})
+imgMiniEsc4.addEventListener("click",function(){
+    imgEscPrin.setAttribute("src","img/image-product-4.jpg")
+    imgEsc4.classList.add("box-img-mini")
+    imgEsc1.classList.remove("box-img-mini")
+    imgEsc2.classList.remove("box-img-mini")
+    imgEsc3.classList.remove("box-img-mini")
+    imgEsc4.classList.add("img-mini-act")
+    imgEsc1.classList.remove("img-mini-act")
+    imgEsc2.classList.remove("img-mini-act")
+    imgEsc3.classList.remove("img-mini-act")
+})
+
+
+let btnAtras = document.getElementById("btnAtras");
+let btnAdelante = document.getElementById("btnAdelante");
+let counter=0;
+let numImg=["img/image-product-1.jpg","img/image-product-2.jpg","img/image-product-3.jpg","img/image-product-4.jpg"];
+
+btnAtras.addEventListener("click", function(){
+    if (counter>0) {
+        counter--;
+    } else {
+        counter = numImg.length-1;
+    }
+    imgEscPrin.setAttribute("src",numImg[counter]);
+})
+
+btnAdelante.addEventListener("click", function(){
+    if (counter<3) {
+       counter++;
+    } else {
+        counter=0;
+    }
+   imgEscPrin.setAttribute("src",numImg[counter]);
+})
 
 
 
