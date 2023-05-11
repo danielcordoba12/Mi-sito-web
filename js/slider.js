758bedfe2d2942bdd3f895ca1e7234575b7624ee
danielcordoba12 <dcordobaruizv@gmail.com>
2023-05-11 11:37:24 -0500

    slider-1.3

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,16 @@
-let leftAroww
+let leftArrow = document.getElementById("leftArrow");
+let imgArbol = document.getElementById("imgArbol");
+let imgForma = document.getElementById("imgForma");
+
+leftArrow.addEventListener("click", function(){
+    if (imgArbol.classList.contains("img-1")){
+        imgArbol.classList.remove("img-1");
+        imgArbol.classList.add("img-1-hidden")
+        imgForma.classList.add("img-2")
+        imgForma.classList.remove("img-2-hidden")
+        
+    }
+});
+// leftArrow.addEventListener("click", function(){
+//     alert (" Funcionando")
+// })
