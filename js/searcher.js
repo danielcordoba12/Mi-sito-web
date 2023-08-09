6df761adf5e5a7bea5eea494a52e13bd8455295e
katiuskizzz <munozkatalinavalderrama@gmail.com>
2023-08-09 11:39:59 -0500

    home center incompleto

--- js/searcher.js
+++ js/searcher.js
@@ -1,5 +1,13 @@
 let button = document.getElementById("button");
+let containerSeacher = document.getElementById("containerSeacher");
+let containerQuery = document.getElementById("containerQuery");
+let container = document.getElementById("container");
 
-// button.addEventListener("click",function(){
-//     alert("Si funciono");
-// });
+
+button.addEventListener("click",function(){
+    container.classList.add("main-searcher-query");
+    container.classList.remove("main-searcher-query-hidden");
+    container.classList.remove("container-searcher");
+
+
+});
