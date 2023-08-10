8f427d299b89706af0bff22382c79e5c884ccc0b
danielcordoba12 <dcordobaruiz@gmail.com>
2023-08-09 22:15:44 -0500

    home center

--- js/searcher.js
+++ js/searcher.js
@@ -1,13 +1,14 @@
 let button = document.getElementById("button");
+let buttonX = document.getElementById("buttonX");
 let containerSeacher = document.getElementById("containerSeacher");
 let containerQuery = document.getElementById("containerQuery");
 let container = document.getElementById("container");
+let mainSearcher = document.getElementById("mainSearcher");
 
 
 button.addEventListener("click",function(){
-    container.classList.add("main-searcher-query");
-    container.classList.remove("main-searcher-query-hidden");
-    container.classList.remove("container-searcher");
-
-
+    containerQuery.classList.remove("main-searcher-query-hidden");
 });
+buttonX.addEventListener("click",function(){
+    containerQuery.classList.add("main-searcher-query-hidden");
+})
