727331dd5842ae48a76d6cc5c49e91e699a1ceea
danielcordoba12 <dcordobaruizv@gmail.com>
2023-08-17 11:44:01 -0500

    chat-incompleto

--- js/searcher.js
+++ js/searcher.js
@@ -4,6 +4,12 @@ let containerSeacher = document.getElementById("containerSeacher");
 let containerQuery = document.getElementById("containerQuery");
 let container = document.getElementById("container");
 let mainSearcher = document.getElementById("mainSearcher");
+let chat = document.getElementById("chat");
+let chatX = document.getElementById("chatX")
+let containChat = document.getElementById("containChat");
+let containChatX = document.getElementById("containChatX");
+let mainContaint = document.getElementById("mainContaint");
+let chatModal = document.getElementById("chatModal");
 
 
 button.addEventListener("click",function(){
@@ -11,4 +17,17 @@ button.addEventListener("click",function(){
 });
 buttonX.addEventListener("click",function(){
     containerQuery.classList.add("main-searcher-query-hidden");
+});
+
+chat.addEventListener("click",function(){
+    containChat.classList.remove("containt-chat");
+    containChat.classList.add("contain-chat-hidden");
+    containChatX.classList.remove("containt-chat-x-hidden")
+    chatModal.classList.remove("chat-modal-hidden")
+});
+chatX.addEventListener("click",function(){
+    containChat.classList.add("containt-chat");
+    containChat.classList.remove("contain-chat-hidden");
+    containChatX.classList.add("containt-chat-x-hidden")
+    chatModal.classList.add("chat-modal-hidden")
 })
