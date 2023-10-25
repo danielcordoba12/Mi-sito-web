c8c7609d00f3d9ad0e73c249e28dbf6ed02b9f49
danielcordoba12 <dcordobaruiz@gmail.com>
2023-10-25 06:10:10 -0500

    avance reloj

--- reloj/launch-countdown-timer-main/js/man.js
+++ reloj/launch-countdown-timer-main/js/man.js
@@ -1,7 +1,24 @@
 let btn = document.getElementById("btn");
+let seconds = document.getElementById("seconds");
+let secondsText = seconds.textContent
+let secondsInt = parseInt(secondsText);
 
 
-btn.addEventListener("click",function()
+function reloj(){
 
-    
-)
+    if (secondsInt >= 0) {
+        console.log(secondsInt);
+        seconds.innerHTML = secondsInt;
+        secondsInt--; // Restar 1 cada vez que se ejecuta
+
+        if (secondsInt < 0) {
+            clearInterval(intervalo); // Detener el intervalo cuando llegue a 5 o menos
+        }
+    }
+}
+
+
+btn.addEventListener("click", function() {
+    // Ejecutar la función "reloj" cada segundo
+    var intervalo = setInterval(reloj, 1000);
+});
