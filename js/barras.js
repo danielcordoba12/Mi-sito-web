229ea4622acc58ddd4d13c20700155ce8ece1161
danielcordoba12 <dcordobaruizv@gmail.com>
2023-11-30 07:40:52 -0500

    arreglo de barra

--- js/barras.js
+++ js/barras.js
@@ -2,6 +2,10 @@ const button = document.getElementById("button");
 const opinion = document.querySelectorAll(".opinion");
 const barras = document.querySelectorAll(".barra-inside");
 const porcentajes = document.querySelectorAll(".porcentaje");
+const votoTotales = document.getElementById("voosTotales");
+const votos = document.getElementById("votos");
+
+
 
 button.addEventListener("click",function(){
 
@@ -18,17 +22,19 @@ button.addEventListener("click",function(){
         return; 
     }  
 
-    let total = 0 ;
+    let total = 0 
     for (a = 0; a < opinion.length; a++ ){
         if( opinion[a].value == "null" || opinion[a].value == ""){
             opinion[a].value = 0;
         }
         total += parseInt(opinion[a].value);
         }
+        votos.innerText = "   "+ total.toString();
     for (let i = 0; i < opinion.length; i++) {
         let  opinionInt = parseInt(opinion[i].value)
         let porcentajeValue = (opinionInt * 100) / total;
-        let porcentajeInt = Math.floor(porcentajeValue) 
+        let porcentajeInt = Math.floor(porcentajeValue);
+
         barras[i].style.backgroundColor = "blue";
         barras[i].style.width = porcentajeInt + "%";
         porcentajes[i].innerHTML = porcentajeInt + "%";
